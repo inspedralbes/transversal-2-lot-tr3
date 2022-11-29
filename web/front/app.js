1c5e58c25194a4c993c34c8a8251e3e29a7bad9b
al20jovgarfra <al20jovgarfra@inspedralbes.cat>
2022-11-29 14:13:59 +0100

    question component and fetch to external api

--- web/front/app.js
+++ web/front/app.js
@@ -1,12 +1,79 @@
+//Componentes
+const Questions={
+    props:true,
+    data:function(){
+        return{
+            quizz:null,
+            correct:0,
+            time:0,
+            nQuestion:0
+        }
+    },
+    methods:{
+        
+    },
+    computed: {
+        isLogged() {
+            return userStore().logged;
+        }
+    },
+    mounted(){
+        if(!userStore().logged){
+            // fetch a demo
+            fetch('https://the-trivia-api.com/api/questions?categories=film_and_tv&limit=10&difficulty=easy')
+            .then ((response)=>response.json())
+            .then((data)=>{
+                this.quizz=data;
+            });
+            console.log('fetch');
+        }else if(type=='daily'){
+            //fetch a diaria
+        }else{
+            //fetch a la api externa 
+        }
+    },
+    template:`
+    <b-container>
+        <div v-if="this.quizz">
+            <question :question_info=this.quizz[this.nQuestion]></question>
+        </div>
+    </b-container>`
+
+}
+
+{/* <div class="card">
+            <div class="card__pregunta">{{question_info.}}</div>
+            <div class="card__respuestas respuesta">
+                <div class="respuestas_respuesta"></div>
+                <div class="respuestas_respuesta"></div>
+                <div class="respuestas_respuesta"></div>
+                <div class="respuestas_respuesta"></div>
+            </div>
+        </div> */}
+
+Vue.component('question',{
+    props: ['question_info'],
+    methods:{
+        validate(){
+        },
+    },
+    template:`<div>
+        {{question_info}}
+    </div>`
+    // template:`<p>{{info.Title}}</p>`
+    
+});
+
+
 //Rutas
 const routes = [
     {
         path: '/',
-        component: Buscador
+        component: Questions
     }, 
     {
-        path: '/personal',
-        component: Personal
+        path: '/questions',
+        component: Questions
     }, 
 ]
 
@@ -39,6 +106,8 @@ const pinia = Pinia.createPinia();
 
 //Bootstrap
 Vue.use(BootstrapVue);
+
+
 
 //app
 var app = new Vue({
