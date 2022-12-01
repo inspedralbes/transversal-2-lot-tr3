9ad585c7179534d64adf0047ffa29473ffbbd38c
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2022-12-01 12:30:23 +0100

    Added game played with user id to the database

--- web/back/app/Http/Controllers/Users_quizzsController.php
+++ web/back/app/Http/Controllers/Users_quizzsController.php
@@ -8,21 +8,5 @@ use App\Models\Quizz;
 
 class Users_quizzsController extends Controller
 {
-    public function newGame(Request $request) {
-        json_decode($request);
-        $addQuizz = new Quizz();
-        $addQuizz -> game = json_encode($request -> json);
-        $addQuizz -> date_creation = date('Y-m-d');
-        $addQuizz -> name_creator = $request -> user_name;
-        $addQuizz -> difficulty = $request -> difficulty;
-        $addQuizz -> category = $request -> category;
-        $addQuizz -> type = 'normal';
-        $addQuizz -> save();
-
-        $addNewGame = new Users_quizz();
-        $addNewGame -> user_id = $request -> id_user;
-        $addNewGame -> quizz_id = $addQuizz -> id;
-        $addNewGame -> date = date('Y-m-d');
-        $addNewGame -> save();
-    }
+    //
 }
