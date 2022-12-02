3d75d0a4157bc7f852837e29249fe68659c377c4
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2022-12-02 13:24:19 +0100

    Fixed merge conflicts

--- web/back/app/Http/Controllers/QuizzsController.php
+++ web/back/app/Http/Controllers/QuizzsController.php
@@ -32,7 +32,7 @@ class QuizzsController extends Controller
     public function newGame(Request $request) {
         $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://the-trivia-api.com/api/questions?limit=10",
+            CURLOPT_URL => "https://the-trivia-api.com/api/questions?limit=10&categories=".$request -> category."&difficulty=".$request -> difficulty,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
