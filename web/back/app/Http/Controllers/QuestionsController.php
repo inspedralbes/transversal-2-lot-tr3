076808536d5235bb42382788a531bb44f2688648
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2022-12-13 14:23:06 +0100

    Prepared method to add questions

--- web/back/app/Http/Controllers/QuestionsController.php
+++ web/back/app/Http/Controllers/QuestionsController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
-    //
+    public function addQuestion(Request $request) {
+        //todo
+        $question = new Question;
+        $question -> question_id = $request -> question_id;
+        if ($request -> correct) {
+            $question -> question_id;
+        }
+        $question -> save();
+        return response()->json($question);
+    }
 }
