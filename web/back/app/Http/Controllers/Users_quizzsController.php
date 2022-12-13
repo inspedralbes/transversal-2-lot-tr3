c30382275895db46bde2ffa695f0c381687eac3e
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2022-12-13 08:52:41 +0100

    Added function to send all quizs from a user

--- web/back/app/Http/Controllers/Users_quizzsController.php
+++ web/back/app/Http/Controllers/Users_quizzsController.php
@@ -12,6 +12,37 @@ class Users_quizzsController extends Controller
         $userQuizzs = Users_quizz::where('user_id', $request -> user_id)
         ->get();
 
+        $allGames = [];
+        foreach ($userQuizzs as $info) {
+            $gameInfo = (object) [
+                'quizz_id' => -1,
+                'score' => -1,
+                'time_resolution' => '',
+                'game' => '',
+                'date_creation' => -1,
+                'name_creator' => '',
+                'difficulty' => '',
+                'category' => '',
+                'type' => '',
+            ];
+
+            $gameInfo -> quizz_id = $info -> quizz_id;
+            $gameInfo -> score = $info -> score;
+            $gameInfo -> time_resolution = $info -> time_resolution;
+
+            $quizzInfo = Quizz::where('id', $gameInfo -> quizz_id)
+            ->first();
+
+            $gameInfo -> game = json_decode($quizzInfo -> game);
+            $gameInfo -> date_creation = $quizzInfo -> date_creation;
+            $gameInfo -> name_creator = $quizzInfo -> name_creator;
+            $gameInfo -> difficulty = $quizzInfo -> difficulty;
+            $gameInfo -> category = $quizzInfo -> category;
+            $gameInfo -> type = $quizzInfo -> type;
+
+            $allGames[] = $gameInfo;
+        }
+
         return response()->json($userQuizzs);
     }
 }
