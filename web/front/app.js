776ddf4fc53e65bb32d81c19b4c769a5f5ada9ec
al20jovgarfra <al20jovgarfra@inspedralbes.cat>
2022-12-15 11:02:28 +0100

    changed function for challenge a friend

--- web/front/app.js
+++ web/front/app.js
@@ -932,7 +932,7 @@ const MyProfile = {
                         <h1>History</h1>
                     </div>
                     <div class="info__content">
-                        <playerHistory :quizzs='quizzs' :challenge='false' @challengeQuizz='challengeFriends'></playerHistory>
+                        <playerHistory :quizzs='quizzs' :challenge='false' @challengeQuizz='seeFriends'></playerHistory>
                     </div>
                 </div>
 
